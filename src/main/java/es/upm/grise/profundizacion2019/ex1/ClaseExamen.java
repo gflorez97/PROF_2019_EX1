7b65d8b1773a027d41b3871a2dbb52efbad551b1
gflorez97 <gflorez97@gmail.com>
2019-10-29 15:58:12 +0100

    Resuelve el examen

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -4,19 +4,24 @@ public class ClaseExamen {
 	
 	public float metodoAProbar(int a, int b) {
 		
+		//nodo 1
 		float result = 0;
-		
 		while( a > b) {
+			//nodo 2
 			a--;
 		}
 		
+		//nodo 3
 		if (a <= b) {
+			//nodo 4
 			result = 0;
 		}
 		else {
+			//nodo 5
 			result = b;
 		}
 		
+		//nodo 6
 		return result;
 	}
 
--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ClaseExamenTest {
+import es.upm.grise.profundizacion2019.ex1.ClaseExamen;
 
+public class ClaseExamenTest {
+	
+	ClaseExamen claseEx = new ClaseExamen();
+
+	//1 3 4 6
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		assertEquals(0, claseEx.metodoAProbar(1, 1), 0);
+	}
+	
+	//1 2 1 3 4 6
+	@Test
+	public void test2() {
+		assertEquals(0, claseEx.metodoAProbar(2, 1), 0);
 	}
 
+	//El tercer caso de prueba no se puede implementar, ver hoja
 }
